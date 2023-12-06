66e5b482a6caa95b93db575b417a5c6c94ccc9ae
zachconnor97 <connorz@oregonstate.edu>
2023-12-06 10:45:20 -0800

    Added TFData

---
+++ AFBM_Utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+
+def encode_labels(labels):
+    #Function to encode labels from input matrix of text to binary matrix encoding
+    encoded_labels = labels
+
+    return encoded_labels
+
+def datastore_generator(data, labels):
+    #Function to generate datastore from input data and encoded labels
+    datastore = data
+
+    return datastore
+
