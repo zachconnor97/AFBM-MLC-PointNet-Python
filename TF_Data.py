fbf7bd4f96ab8fa57745818c04de2cced1b6b715
zachconnor97 <connorz@oregonstate.edu>
2023-12-06 10:57:46 -0800

    Update TF_Data.py

--- TF_Data.py
+++ TF_Data.py
@@ -2,4 +2,7 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-data_file_path = ''
+data_file_path = 'test.csv'
+username = 'connorz'
+cloud_path_header = str('C:/Users/' + username + '/Box/Automated Functional Basis Modeling/ShapeNetCore.v2/AllClouds10k/')
+print(cloud_path_header)
