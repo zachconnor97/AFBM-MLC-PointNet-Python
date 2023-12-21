9f6ad51a622a7932b9c6cdd8eab6a62fff683dd7
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2023-12-20 17:28:30 -0800

    New stuff

--- LayersTest.py
+++ LayersTest.py
@@ -20,15 +20,17 @@ tf.random.set_seed(1234)
 clouds = tf.random.normal([32,5000,3])
 
 inputs = keras.Input(shape=(5000, 3))
-x = layers.Dense(64)(inputs)
-x = layers.BatchNormalization(momentum=0.0)(x)
-x = layers.Activation("relu")(x)
-#print(x)
-x = layers.Dense(128)(x)
+
+# T-Net 1
+x = layers.Conv1D(64, kernel_size=1, padding="valid")(inputs)
 x = layers.BatchNormalization(momentum=0.0)(x)
 x = layers.Activation("relu")(x)
 
-x = layers.Dense(1024)(x)
+x = layers.Conv1D(128, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+x = layers.Conv1D(1024, kernel_size=1, padding="valid")(x)
 x = layers.BatchNormalization(momentum=0.0)(x)
 x = layers.Activation("relu")(x)
 
@@ -43,16 +45,87 @@ x = layers.BatchNormalization(momentum=0.0)(x)
 x = layers.Activation("relu")(x)
 
 x = layers.Dense(
-        3 * 3,
+        9,
         kernel_initializer="zeros",
         bias_initializer = keras.initializers.Constant(np.eye(3).flatten()),
         #activity_regularizer = OrthogonalRegularizer(3),
     )(x)
 
 x = layers.Reshape((3, 3))(x)
-
 x = layers.Dot(axes=(2, 1))([inputs, x])
+
+# MLP 1
+
+x = layers.Conv1D(64, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+x = layers.Conv1D(64, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+# T-Net 2
+x2 = layers.Conv1D(64, kernel_size=1, padding="valid")(x)
+x2 = layers.BatchNormalization(momentum=0.0)(x2)
+x2 = layers.Activation("relu")(x2)
+
+x2 = layers.Conv1D(128, kernel_size=1, padding="valid")(x2)
+x2 = layers.BatchNormalization(momentum=0.0)(x2)
+x2 = layers.Activation("relu")(x2)
+
+x2 = layers.Conv1D(1024, kernel_size=1, padding="valid")(x2)
+x2 = layers.BatchNormalization(momentum=0.0)(x2)
+x2 = layers.Activation("relu")(x2)
+
+x2 = layers.GlobalMaxPooling1D()(x2)
+
+x2 = layers.Dense(512)(x2)
+x2 = layers.BatchNormalization(momentum=0.0)(x2)
+x2 = layers.Activation("relu")(x2)
+
+x2 = layers.Dense(256)(x2)
+x2 = layers.BatchNormalization(momentum=0.0)(x2)
+x2 = layers.Activation("relu")(x2)
+
+x2 = layers.Dense(
+        64*64,
+        kernel_initializer="zeros",
+        bias_initializer = keras.initializers.Constant(np.eye(64).flatten()),
+        #activity_regularizer = OrthogonalRegularizer(3),
+    )(x2)
+
+x2 = layers.Reshape((64, 64))(x2)
 print(x)
+print(x2)
+x = layers.Dot(axes=(2, 1))([x, x2])
+
+# MLP 2
+x = layers.Conv1D(64, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+x = layers.Conv1D(128, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+x = layers.Conv1D(1024, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+x = layers.GlobalMaxPooling1D()(x)
+
+x = layers.Dense(512)(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+x = layers.Dropout(0.3)(x)
+
+x = layers.Dense(256)(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+x = layers.Dropout(0.3)(x)
+
 outputs = layers.Dense(25, activation="softmax")(x)
 model = keras.Model(inputs=inputs,outputs=x)
 model.summary()
