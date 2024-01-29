6bf65b80f2f045e0f30c917133c66c66bd098fc5
zachconnor97 <zachconnor97@gmail.com>
2024-01-29 13:00:31 -0800

    We think we got it???

--- ImportTest.py
+++ ImportTest.py
@@ -20,12 +20,20 @@ df.pop('fileid')
 df.pop('status')
  
 # Point Cloud Read Function
-def pc_read(path):
+def pc_read(path,label):
     #input is a tensor object, needs to become a standard string
     #path = path.numpy().astype('str') #doesn't work
     #path = path.numpy
+    
     print(type(path))
     print(path)
+    try:
+        path.numpy()
+    except:
+        path = 'Ruh Roh Raggy'
+    finally:
+        print(path)
+        print(type(path))
     path = cloud_path_header + path 
     cloud = open3d.io.read_point_cloud(path)
     """
@@ -69,15 +77,49 @@ BATCH_SIZE = 64
 file_paths = np.asmatrix(file_paths)
 nfile_paths = file_paths.reshape((np.size(file_paths),1)) 
 nfile_paths = np.asarray(nfile_paths)
+print(type(nfile_paths.tolist()))
+print(type(sparse_matrix.tolist()))
+tfile_paths = tf.constant(nfile_paths.tolist())
+tsparse = tf.constant(sparse_matrix.tolist())
+print(type(tfile_paths))
+print(type(tsparse))
+fileset_new = tf.data.Dataset.from_tensor_slices((tfile_paths))
+fileset_new.map(lambda x: tf.py_function(pc_read, [x], [tf.float32]))
+
+labelset = tf.data.Dataset.from_tensor_slices((tsparse))
+afbm_dataset = tf.data.Dataset.zip((fileset_new, labelset))
+
+for element in afbm_dataset.as_numpy_iterator():
+    print(element[0])
+# dataset_new.zip(dataset_new, tsparse)
+"""
 sparse_matrix = np.asarray(sparse_matrix.astype('str'))
 zata = np.concatenate((nfile_paths,sparse_matrix),axis=1)
 
+ten_size = zata.shape
+print(ten_size[0])
+
 # NUMPY list not good for tensorflow 
 # https://stackoverflow.com/questions/58636087/tensorflow-valueerror-failed-to-convert-a-numpy-array-to-a-tensor-unsupporte
-train_data = tf.data.Dataset.from_tensor_slices(zata)
+
+#train_data = tf.data.Dataset.from_tensor_slices(zata)
+#train_data.map(pc_read)
+tdata = tf.TensorArray(dtype=tf.string, size=ten_size[0], dynamic_size=True, clear_after_read=False)
+zata = tf.constant(zata, dtype=tf.string)
+
+for i in range(ten_size[0]):
+    tdata = tdata.write(i,zata[i])
+
+train_data = tf.data.Dataset.from_tensor_slices(tdata)
 train_data.map(pc_read)
 
-
+#for i in range(10):
+#    print(tdata.read(i).numpy())
+"""
+"""
+for element in tdata:
+    print(element)
+"""
 #split to training and validation sets
 #val_data = train_data.map()
 #train_data = train_data.map()
