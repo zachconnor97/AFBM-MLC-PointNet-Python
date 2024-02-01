9304df64dc7eb2cebac2404adfd21a57750c15fd
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-02-01 11:01:24 -0800

    Added functions for generating dataset in tf

--- AFBM_MLC.py
+++ AFBM_MLC.py
@@ -11,45 +11,24 @@ import pandas as pd
 
 tf.random.set_seed(1234)
 NUM_POINTS = 2000
+BATCH_SIZE = 32
 username = 'Zachariah'
-cloud_path_header = str('C:/Users/' + username + '/Box/Automated Functional Basis Modeling/ShapeNetCore.v2/AllClouds10k/')
 
-# Import the csv and convert to strings
-df = pd.read_csv("AFBMData_NoChairs.csv")
-df = df.astype('str')
- 
-# Seperates cloud paths to pandas series 
-file_paths = df.pop('cloudpath')
-#print(file_paths)
-# Removes non-necessary dataframe columns to leave just FBM labels
-df.pop('SysetID')
-df.pop('Name')
-df.pop('.obj paths')
-df.pop('fileid')
-df.pop('status')
- 
-# Point Cloud Read Function
 def pc_read(path):
-    #input is a tensor object, needs to become a standard string
-    #path = path.numpy().astype('str') #doesn't work
-    #path = path.numpy
-    
-    print(type(path))
-    print(path)
+    cloud_path_header = str('C:/Users/' + username + '/Box/Automated Functional Basis Modeling/ShapeNetCore.v2/AllClouds10k/')
     try:
-        path.numpy()
+        path = path.numpy()
+        path = np.array2string(path)
+        for character in "[]]b'":
+            path = path.replace(character, '')
     except:
-        path = 'Ruh Roh Raggy.txt'
+        path = 'ERROR IN PCREAD: Transformation from Tensor to String Failed'
+        print(path)
     finally:
         print(path)
         print(type(path))
         path = cloud_path_header + path 
         cloud = open3d.io.read_point_cloud(path)
-    """
-    open3d.visualization.draw_geometries([cloud])
-    cloud = cloud.voxel_down_sample(voxel_size=0.05)
-    open3d.visualization.draw_geometries([cloud])
-    """
     cloud = np.asarray(cloud.points)
     return cloud
 
@@ -76,114 +55,50 @@ def Sparse_Matrix_Encoding(df):
   sparse_matrix = encodedLabel
   return sparse_matrix
 
-sparse_matrix = Sparse_Matrix_Encoding(df) 
-BATCH_SIZE = 64
+def generate_dataset(filename):
 
-# Slice file paths and labels to tf.data.Dataset
-file_paths = np.asmatrix(file_paths)
-nfile_paths = file_paths.reshape((np.size(file_paths),1)) 
-nfile_paths = np.asarray(nfile_paths)
-tfile_paths = tf.constant(nfile_paths.tolist())
-tsparse = tf.constant(sparse_matrix.tolist())
-fileset_new = tf.data.Dataset.from_tensor_slices((tfile_paths))
-fileset_new.map(lambda x: tf.py_function(pc_read, [x], tf.float32)) # map(pc_read)
+    # Import the csv and convert to strings
+    df = pd.read_csv("AFBMData_NoChairs.csv")
+    df = df.astype('str')
+    
+    # Seperates cloud paths to pandas series 
+    file_paths = df.pop('cloudpath')
+    #print(file_paths)
+    # Removes non-necessary dataframe columns to leave just FBM labels
+    df.pop('SysetID')
+    df.pop('Name')
+    df.pop('.obj paths')
+    df.pop('fileid')
+    df.pop('status')
 
-labelset = tf.data.Dataset.from_tensor_slices((tsparse))
-afbm_dataset = tf.data.Dataset.zip((fileset_new, labelset))
-afbm_dataset.batch(BATCH_SIZE)
+    sparse_matrix = Sparse_Matrix_Encoding(df) 
 
-data = afbm_dataset.take(1)
-points, labels = list(data)[0]
+    # Slice file paths and labels to tf.data.Dataset
+    file_paths = np.asmatrix(file_paths)
+    nfile_paths = file_paths.reshape((np.size(file_paths),1)) 
+    nfile_paths = np.asarray(nfile_paths)
+    tfile_paths = tf.constant(nfile_paths.tolist())
+    tsparse = tf.constant(sparse_matrix.tolist())
+    fileset_new = tf.data.Dataset.from_tensor_slices((tfile_paths))
 
+    fileset_new = fileset_new.map(lambda x: tf.py_function(pc_read, [x], tf.float64))
 
-#DATA_DIR = tf.keras.utils.get_file(
-#    "modelnet.zip",
-#    "http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip",
-#    extract=True,
-#)
-#DATA_DIR = os.path.join(os.path.dirname(DATA_DIR), "ModelNet10")
+    labelset = tf.data.Dataset.from_tensor_slices((tsparse))
+    afbm_dataset = tf.data.Dataset.zip((fileset_new, labelset))
 
-"""We can use the `trimesh` package to read and visualize the `.off` mesh files.
+    #Testing stuff
+    """
+    data = afbm_dataset.take(1)
+    points, labels = list(data)[0]
+    #print(labels)
+    print(points.numpy())
+    print(type(points.numpy()))
+    pcd = open3d.geometry.PointCloud()
+    pcd.points = open3d.utility.Vector3dVector(points.numpy())
+    open3d.visualization.draw_geometries([pcd])
+    """
+    return afbm_dataset
 
-"""
-
-#mesh = trimesh.load(os.path.join(DATA_DIR, "chair/train/chair_0001.off"))
-#mesh.show() # does not work with WSL 
-
-"""To convert a mesh file to a point cloud we first need to sample points on the mesh
-surface. `.sample()` performs a unifrom random sampling. Here we sample at 2048 locations
-and visualize in `matplotlib`.
-
-"""
-
-#points = mesh.sample(5000)
-
-#fig = plt.figure(figsize=(5, 5))
-#ax = fig.add_subplot(111, projection="3d")
-#ax.scatter(points[:, 0], points[:, 1], points[:, 2])
-#ax.set_axis_off()
-#plt.show()
-
-"""To generate a `tf.data.Dataset()` we need to first parse through the ModelNet data
-folders. Each mesh is loaded and sampled into a point cloud before being added to a
-standard python list and converted to a `numpy` array. We also store the current
-enumerate index value as the object label and use a dictionary to recall this later.
-
-"""
-"""
-def parse_dataset(num_points=5000):
-
-    train_points = []
-    train_labels = []
-    test_points = []
-    test_labels = []
-    class_map = {}
-    folders = glob.glob(os.path.join(DATA_DIR, "[!README]*"))
-
-    for i, folder in enumerate(folders):
-        print("processing class: {}".format(os.path.basename(folder)))
-        # store folder name with ID so we can retrieve later
-        class_map[i] = folder.split("/")[-1]
-        # gather all files
-        train_files = glob.glob(os.path.join(folder, "train/*"))
-        test_files = glob.glob(os.path.join(folder, "test/*"))
-
-        for f in train_files:
-            train_points.append(trimesh.load(f).sample(num_points))
-            train_labels.append(i)
-
-        for f in test_files:
-            test_points.append(trimesh.load(f).sample(num_points))
-            test_labels.append(i)
-
-    return (
-        np.array(train_points),
-        np.array(test_points),
-        np.array(train_labels),
-        np.array(test_labels),
-        class_map,
-    )
-"""
-"""Set the number of points to sample and batch size and parse the dataset. This can take
-~5minutes to complete.
-
-"""
-"""
-NUM_POINTS = 5000
-NUM_CLASSES = 10
-BATCH_SIZE = 32
-
-train_points, test_points, train_labels, test_labels, CLASS_MAP = parse_dataset(
-    NUM_POINTS
-)
-"""
-"""Our data can now be read into a `tf.data.Dataset()` object. We set the shuffle buffer
-size to the entire size of the dataset as prior to this the data is ordered by class.
-Data augmentation is important when working with point cloud data. We create a
-augmentation function to jitter and shuffle the train dataset.
-
-"""
-"""
 def augment(points, label):
     # jitter points
     points += tf.random.uniform(points.shape, -0.005, 0.005, dtype=tf.float64)
@@ -191,20 +106,38 @@ def augment(points, label):
     points = tf.random.shuffle(points)
     return points, label
 
+database = "AFBMData_NoChairs.csv"
+afbm_dataset = generate_dataset(filename=database)
+#Seperate into training and validation here:
+# train_data = 
+# val_data =
 
+# Sample points down to NUM_POINTS
+# Use trimesh probably? 
+# Write new function
+
+# Batch data and augment train data. Also Shuffle
+#train_data.batch(BATCH_SIZE).map(augment)
+#val_data.batch(BATCH_SIZE)
+
+#Also shuffle points
+# dataset.shuffle(len(train_points))
+
+
+"""
 train_dataset = tf.data.Dataset.from_tensor_slices((train_points, train_labels))
 test_dataset = tf.data.Dataset.from_tensor_slices((test_points, test_labels))
 
 train_dataset = train_dataset.shuffle(len(train_points)).map(augment).batch(BATCH_SIZE)
 test_dataset = test_dataset.shuffle(len(test_points)).batch(BATCH_SIZE)
+"""
 
-"""### Build a model
+### Build a model
 
 #Each convolution and fully-connected layer (with exception for end layers) consits of
 #Convolution / Dense -> Batch Normalization -> ReLU Activation.
 
-"""
-"""
+
 def conv_bn(x, filters):
     x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
     x = layers.BatchNormalization(momentum=0.0)(x)
@@ -303,7 +236,7 @@ model.compile(
     metrics=["sparse_categorical_accuracy"],
 )
 
-model.fit(data, epochs=5)
+model.fit(afbm_dataset, epochs=5)
 
 """## Visualize predictions
 
