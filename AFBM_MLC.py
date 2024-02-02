8a021356e0b0f415d15619c232bee504000161cc
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-02-02 08:13:31 -0800

    import

--- AFBM_MLC.py
+++ AFBM_MLC.py
@@ -4,7 +4,7 @@ import trimesh
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from keras import layers
 from matplotlib import pyplot as plt
 import open3d
 import pandas as pd
