3b5dcd483a9894f636eb5cfbac4b4ffa46b052b7
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-02-05 13:00:15 -0800

    argg

--- ImportTest.py
+++ ImportTest.py
@@ -1,28 +1,45 @@
-import tensorflow as tf
-import pandas as pd
+import os
+import glob
+import trimesh
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from matplotlib import pyplot as plt
 import open3d
- 
+import pandas as pd
+import pymeshlab
+
+tf.random.set_seed(1234)
+NUM_POINTS = 3000
+BATCH_SIZE = 32
+NUM_CLASSES = 25
 username = 'Zachariah'
 
 def pc_read(path):
+    
     #cloud_path_header = str('C:/Users/' + username +'/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/')
+    # Use second one for WSL
     cloud_path_header = str('/mnt/c/Users/' + username +'/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/')
     try:
         path = path.numpy()
         path = np.array2string(path)
-        for character in "[]'":
+        for character in "[]]'":
             path = path.replace(character, '')
         path = path[1:]
+        path = cloud_path_header + path 
+        cloud = open3d.io.read_point_cloud(path)
+        cloud = cloud.uniform_down_sample(cloud, 2)
+        #cloud = trimesh.load(path)
+        #cloud = cloud.sample(NUM_POINTS) # that don't work
+        #cloud = pymeshlab.load_new_mesh(path)
     except:
+        cloud = np.random.rand((NUM_POINTS,3))
         path = 'ERROR IN PCREAD: Transformation from Tensor to String Failed'
         print(path)
     finally:
-        print(path)
-        print(type(path))
-        path = cloud_path_header + path 
-        cloud = open3d.io.read_point_cloud(path)
-    cloud = np.asarray(cloud.points)
+        cloud = np.asarray(cloud.points())
+    
     return cloud
 
 # ISparse Matrix Encoding Function
@@ -48,7 +65,14 @@ def Sparse_Matrix_Encoding(df):
   sparse_matrix = encodedLabel
   return sparse_matrix
 
-def generate_dataset(username, batch_size):
+def augment(points, label):
+    # jitter points
+    points += tf.random.uniform(points.shape, -0.005, 0.005, dtype=tf.float64)
+    # shuffle points
+    points = tf.random.shuffle(points)
+    return points, label
+
+def generate_dataset(filename):
 
     # Import the csv and convert to strings
     df = pd.read_csv("AFBMData_NoChairs.csv")
@@ -65,7 +89,6 @@ def generate_dataset(username, batch_size):
     df.pop('status')
 
     sparse_matrix = Sparse_Matrix_Encoding(df) 
-    BATCH_SIZE = 64
 
     # Slice file paths and labels to tf.data.Dataset
     file_paths = np.asmatrix(file_paths)
@@ -73,25 +96,53 @@ def generate_dataset(username, batch_size):
     nfile_paths = np.asarray(nfile_paths)
     tfile_paths = tf.constant(nfile_paths.tolist())
     tsparse = tf.constant(sparse_matrix.tolist())
-    fileset_new = tf.data.Dataset.from_tensor_slices((tfile_paths))
-
-    fileset_new = fileset_new.map(lambda x: tf.py_function(pc_read, [x], tf.float64))
-
+    fileset = tf.data.Dataset.from_tensor_slices((tfile_paths))
     labelset = tf.data.Dataset.from_tensor_slices((tsparse))
-    afbm_dataset = tf.data.Dataset.zip((fileset_new, labelset))
+    
+    train_points = fileset.skip(int(0.3*len(fileset)))
+    train_label = labelset.skip(int(.3*len(labelset)))
+    
+    val_points = fileset.take(int(0.3*len(fileset)))
+    val_label = labelset.take(int(.3*len(labelset)))
+    
+    val_points = val_points.map(lambda x: tf.py_function(pc_read, [x], tf.float64))
+    train_points = train_points.map(lambda x: tf.py_function(pc_read, [x], tf.float64))
+
+    
+    val_ds = tf.data.Dataset.zip((val_points, val_label))
+    train_ds = tf.data.Dataset.zip((train_points, train_label))
+
+    #afbm_dataset = tf.data.Dataset.zip((fileset_new, labelset))
+
+    #train_ds, val_ds = tf.keras.utils.split_dataset(afbm_dataset, left_size=0.7)
+    #val_ds = afbm_dataset.take(int(.3*len(afbm_dataset)))
+    #train_ds = afbm_dataset.skip(int(0.3*len(afbm_dataset)))
+
+    #val_ds = val_ds.map(lambda x, y: tf.py_function(pc_read, [x, y], tf.float64))
+    #train_ds = train_ds.map(lambda x, y: tf.py_function(pc_read, [x, y], tf.float64))
+
+    #val_ds = val_ds.batch(BATCH_SIZE)
+    #train_ds = train_ds.batch(BATCH_SIZE)
+    #.map(lambda x, y: tf.py_function(augment, [x, y], tf.float64))
 
     #Testing stuff
-    
-    data = afbm_dataset.take(20)
-    points, labels = list(data)[17]
-    print(labels.numpy())
+    """
+    data = afbm_dataset.take(1)
+    points, labels = list(data)[0]
+    #print(labels)
     print(points.numpy())
     print(type(points.numpy()))
     pcd = open3d.geometry.PointCloud()
     pcd.points = open3d.utility.Vector3dVector(points.numpy())
     open3d.visualization.draw_geometries([pcd])
-    
-    return afbm_dataset
+    """
+    return train_ds, val_ds
 
-if __name__=="__main__":
-    afbm_dataset = generate_dataset(username=username,batch_size=64)
+
+
+database = "AFBMData_NoChairs.csv"
+train_ds, val_ds = generate_dataset(filename=database)
+
+test = train_ds.take(1)
+points, labels = list(test)[0]
+print(points)
