67c5a058ee3e8a028d1a38ba463b744cb7668175
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-02-08 08:44:45 -0800

    Running stuff in WSL

--- AFBM_MLC.py
+++ AFBM_MLC.py
@@ -21,9 +21,9 @@ username = 'Zachariah'
 
 def pc_read(path):
     
-    cloud_path_header = str('C:/Users/' + username +'/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/')
+    #cloud_path_header = str('C:/Users/' + username +'/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/')
     # Use second one for WSL
-    #cloud_path_header = str('/mnt/c/Users/' + username +'/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/')
+    cloud_path_header = str('/mnt/c/Users/' + username +'/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/')
     try:
         path = path.numpy()
         path = np.array2string(path)
