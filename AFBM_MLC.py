91bcf19b3b0dda067065287427f1fad26f8c8b46
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-02-09 11:37:51 -0800

    yeah

--- AFBM_MLC.py
+++ AFBM_MLC.py
@@ -12,6 +12,7 @@ import pandas as pd
 from datetime import date
 import sys
 physical_devices = tf.config.list_physical_devices('GPU')
+print(physical_devices)
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 tf.random.set_seed(1234)
