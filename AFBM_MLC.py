3c85d64500b5d48975a00894f5860b74bc580afc
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-02-09 08:40:39 -0800

    xfzg

--- AFBM_MLC.py
+++ AFBM_MLC.py
@@ -272,29 +272,34 @@ print(predc)
 train_stuff = model.fit(x=train_ds, epochs=10, validation_data=val_ds, class_weight=label_weights)
 #model.evaluate()
 print(train_stuff)
+
+
+
+"""
 # Visualize predictions
 
 #We can use matplotlib to visualize our trained model performance.
-#data = test_dataset.take(1)
-#points, labels = list(data)[0]
+data = train_ds.take(1)
+points, labels = list(data)[0]
 #points = points[:8, ...]
 #labels = labels[:8, ...]
 
 # run test data through model
-#preds = model.predict(points)
-#preds = tf.math.argmax(preds, -1)
+preds = model.predict(points)
+preds = tf.math.argmax(preds, -1)
 
-#points = points.numpy()
+points = points.numpy()
 
 # plot points with predicted class and label
-#fig = plt.figure(figsize=(15, 10))
-#for i in range(8):
-#    ax = fig.add_subplot(2, 4, i + 1, projection="3d")
-#    ax.scatter(points[i, :, 0], points[i, :, 1], points[i, :, 2])
-#    ax.set_title(
-#        "pred: {:}, label: {:}".format(
-#            CLASS_MAP[preds[i].numpy()], CLASS_MAP[labels.numpy()[i]]
-#        )
-#    )
-#    ax.set_axis_off()
-#plt.show()
+fig = plt.figure(figsize=(15, 10))
+for i in range(8):
+    ax = fig.add_subplot(2, 4, i + 1, projection="3d")
+    ax.scatter(points[i, :, 0], points[i, :, 1], points[i, :, 2])
+    ax.set_title(
+        "pred: {:}, label: {:}".format(
+            CLASS_MAP[preds[i].numpy()], CLASS_MAP[labels.numpy()[i]]
+        )
+    )
+    ax.set_axis_off()
+plt.show()
+"""
