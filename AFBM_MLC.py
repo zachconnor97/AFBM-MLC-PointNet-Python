708f10dcab79d9905c969faa58e9cd984d07ee6a
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-02-09 17:21:04 -0800

    Fixed Label Evaluation

--- AFBM_MLC.py
+++ AFBM_MLC.py
@@ -15,13 +15,13 @@ print(physical_devices)
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 tf.random.set_seed(1234)
-NUM_POINTS = 200
+NUM_POINTS = 2000
 SAMPLE_RATIO = int(10000 / NUM_POINTS)
 print("Sample Ratio:")
-print(SAMPLE_RATIO)
+print(1/SAMPLE_RATIO)
 BATCH_SIZE = 32
 NUM_CLASSES = 25
-NUM_EPOCHS = 1
+NUM_EPOCHS = 15
 username = 'Zachariah'
 
 class GarbageMan(tf.keras.callbacks.Callback):
@@ -268,56 +268,8 @@ model.compile(
     loss=tf.keras.losses.BinaryCrossentropy(),
     optimizer=keras.optimizers.Adam(learning_rate=0.002),
     metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0.5),
-             tf.keras.metrics.Precision(class_id=0),
-             tf.keras.metrics.Precision(class_id=1),
-             tf.keras.metrics.Precision(class_id=2),
-             tf.keras.metrics.Precision(class_id=3),
-             tf.keras.metrics.Precision(class_id=4),
-             tf.keras.metrics.Precision(class_id=5),
-             tf.keras.metrics.Precision(class_id=6),
-             tf.keras.metrics.Precision(class_id=7),
-             tf.keras.metrics.Precision(class_id=8),
-             tf.keras.metrics.Precision(class_id=9),
-             tf.keras.metrics.Precision(class_id=10),
-             tf.keras.metrics.Precision(class_id=11),
-             tf.keras.metrics.Precision(class_id=12),
-             tf.keras.metrics.Precision(class_id=13),
-             tf.keras.metrics.Precision(class_id=14),
-             tf.keras.metrics.Precision(class_id=15),
-             tf.keras.metrics.Precision(class_id=16),
-             tf.keras.metrics.Precision(class_id=17),
-             tf.keras.metrics.Precision(class_id=18),
-             tf.keras.metrics.Precision(class_id=19),
-             tf.keras.metrics.Precision(class_id=20),
-             tf.keras.metrics.Precision(class_id=21),
-             tf.keras.metrics.Precision(class_id=22),
-             tf.keras.metrics.Precision(class_id=23),
-             tf.keras.metrics.Precision(class_id=24),
-             tf.keras.metrics.Recall(class_id=0),
-             tf.keras.metrics.Recall(class_id=1),
-             tf.keras.metrics.Recall(class_id=2),
-             tf.keras.metrics.Recall(class_id=3),
-             tf.keras.metrics.Recall(class_id=4),
-             tf.keras.metrics.Recall(class_id=5),
-             tf.keras.metrics.Recall(class_id=6),
-             tf.keras.metrics.Recall(class_id=7),
-             tf.keras.metrics.Recall(class_id=8),
-             tf.keras.metrics.Recall(class_id=9),
-             tf.keras.metrics.Recall(class_id=10),
-             tf.keras.metrics.Recall(class_id=11),
-             tf.keras.metrics.Recall(class_id=12),
-             tf.keras.metrics.Recall(class_id=13),
-             tf.keras.metrics.Recall(class_id=14),
-             tf.keras.metrics.Recall(class_id=15),
-             tf.keras.metrics.Recall(class_id=16),
-             tf.keras.metrics.Recall(class_id=17),
-             tf.keras.metrics.Recall(class_id=18),
-             tf.keras.metrics.Recall(class_id=19),
-             tf.keras.metrics.Recall(class_id=20),
-             tf.keras.metrics.Recall(class_id=21),
-             tf.keras.metrics.Recall(class_id=22),
-             tf.keras.metrics.Recall(class_id=23),
-             tf.keras.metrics.Recall(class_id=24),
+             tf.keras.metrics.Precision(thresholds=[0.5,1]),
+             tf.keras.metrics.Recall(thresholds=[0.5,1]),
              tf.keras.metrics.F1Score(threshold=0.5),
              tf.keras.metrics.IoU(num_classes=NUM_CLASSES, target_class_ids=list(range(0,25)))],      
     run_eagerly=True,
@@ -328,18 +280,17 @@ points, labels = list(train_data)[0]
 predc = model.predict(points)
 print(predc)
 """
-"""
+
 train_hist = model.fit(x=train_ds, epochs=NUM_EPOCHS, class_weight=label_weights, validation_data=val_ds, callbacks=[GarbageMan()])
-## Save Model here
-#model.save(save_path + '_AFBM Model')
 
 #Save history file
 histdf = pd.DataFrame(train_hist.history)
 histfile = save_path + '_history.csv'
 with open(histfile, mode='w') as f:
     histdf.to_csv(f)
-"""
 
+## Save Model here
+#model.save(save_path + '_AFBM Model')
 
 ## Load Model here
 #model = tf.keras.models.load_model(save_path)
@@ -348,29 +299,24 @@ with open(histfile, mode='w') as f:
 #model.summary()
 
 
-# Validation / Evaluation
- 
+# Validation / Evaluation per Label
 for i in range(0,NUM_CLASSES-1):
     model.compile(
         loss=tf.keras.losses.BinaryCrossentropy(),
         optimizer=keras.optimizers.Adam(learning_rate=0.002),
         metrics=[
-            tf.keras.metrics.BinaryAccuracy(threshold=0.5),
-            tf.keras.metrics.Precision(class_id=i),
-            tf.keras.metrics.Recall(class_id=i),
+            tf.keras.metrics.Precision(thresholds=[0.5, 1],class_id=i),
+            tf.keras.metrics.Recall(thresholds=[0.5, 1],class_id=i),
             tf.keras.metrics.F1Score(threshold=0.5),
-            #tf.keras.metrics.IoU(num_classes=1,target_class_ids=[i]),      
+            tf.keras.metrics.IoU(num_classes=25,target_class_ids=[i]),      
         ],
         run_eagerly=True,
     )
     data = model.evaluate(x=val_ds)
-    print(type(data))
-    print(data)
     histdf = pd.DataFrame(data)
     histfile = save_path + '_Label' + str(i+1) + '_evaluatation.csv'
     with open(histfile, mode='w') as f:
         histdf.to_csv(f)
-
 
 """
 # Visualize predictions
