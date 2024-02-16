77029a7bdf543616331a7998ad8c4876ee1554b5
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-02-16 13:53:08 -0800

    Updated Code

--- AFBM_MLC.py
+++ AFBM_MLC.py
@@ -82,24 +82,17 @@ class PerLabelMetricCallBack(tf.keras.callbacks.Callback):
         correct = 0
         incorrect = 0
         x_result = self.model.predict(x_data, verbose=0)
-
         x_numpy = []
-
         for i in labels:
             self.class_history.append([])
-
         class_correct = [0] * len(labels)
         class_incorrect = [0] * len(labels)
-
         for i in range(len(x_data)):
             x = x_data[i]
             y = y_data[i]
-
             res = x_result[i]
-
             actual_label = np.argmax(y)
             pred_label = np.argmax(y)
-
             if(pred_label == actual_label):
                 x_numpy.append(["cor:", str(y), str(res), str(pred_label)])
                 class_correct[actual_label] += 1
@@ -110,7 +103,6 @@ class PerLabelMetricCallBack(tf.keras.callbacks.Callback):
                 incorrect += 1
         print("\tCorrect: %d" %(correct))
         print("\tIncorrect: %d" %(incorrect))
-
         for i in range(len(labels)):
             tot = float(class_correct[i] + class_incorrect[i])
             class_acc = -1
@@ -118,7 +110,6 @@ class PerLabelMetricCallBack(tf.keras.callbacks.Callback):
                 class_acc = float(class_correct[i]) / tot
             print("\t%s: %.3f" %(class_correct[i], class_acc))
         acc = float(correct) / float(correct + incorrect)
-
         print("\tCurrent Network Accuracy: %.3f" %(acc))
 
 def pc_read(path):
