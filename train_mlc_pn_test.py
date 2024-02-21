b52b15665f00aaa80416af9503dada9eb7c75ed0
zachconnor97 <zachconnor97@gmail.com>
2024-02-21 11:02:13 -0800

    Updated Code

--- train_mlc_pn_test.py
+++ train_mlc_pn_test.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-from model import pointnet
+from model import pointnet, generator
 from AFBM_MLC import PerLabelMetric, generate_dataset, GarbageMan
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -25,11 +25,25 @@ pn.compile(
             tf.keras.metrics.BinaryAccuracy(threshold=0.5),
             tf.keras.metrics.Precision(thresholds=[0.5,1]),
             tf.keras.metrics.Recall(thresholds=[0.5,1]),
-            tf.keras.metrics.F1Score(threshold=0.5)
+            tf.keras.metrics.F1Score(threshold=0.5),
             ],      
+    run_eagerly=True,
+)
+
+gen = generator(num_points=NUM_POINTS,num_classes=NUM_CLASSES,training=TRAINING)
+gen.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=LEARN_RATE),
+    metrics=[
+        PerLabelMetric(num_labels=NUM_CLASSES),
+        tf.keras.metrics.BinaryAccuracy(threshold=0.5)
+        tf.keras.metrics.Precision(thresholds=[0.5,1]),
+        tf.keras.metrics.Recall(thresholds=[0.5,1]),
+            tf.keras.metrics.F1Score(threshold=0.5),
+    ],
     run_eagerly=True,
 )
 
 #thist = pn.fit(x=train_ds, epochs=NUM_EPOCHS, class_weight=label_weights, validation_data=val_ds, callbacks=[GarbageMan()])
 
-val_data = pn.evaluate(x=val_ds)
+#val_data = pn.evaluate(x=val_ds)
