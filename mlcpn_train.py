97c42fe9ca8e7fb514c18e61a17ef429d10407e2
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-02-21 18:10:10 -0800

    updated code

--- mlcpn_train.py
+++ mlcpn_train.py
@@ -10,12 +10,12 @@ import csv
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
-NUM_POINTS = 5000
+NUM_POINTS = 2000
 NUM_CLASSES = 25
 TRAINING = True
 LEARN_RATE = 0.0003
-BATCH_SIZE = 16
-NUM_EPOCHS = 10
+BATCH_SIZE = 32
+NUM_EPOCHS = 25
 username = 'Zachariah'
 database = "AFBMData_NoChairs_Augmented.csv"
 save_path = str('/mnt/c/Users/' + username +'/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AFBMGit/AFBM_TF_DATASET/' + str(date.today()) + '_' + str(BATCH_SIZE) + '_' + str(NUM_POINTS) + '_' + str(NUM_EPOCHS) + '_' + 'Learning Rate_' + str(LEARN_RATE))
