854ab731257cfad9b9b63f2e1d5f6ab7c0dcacba
shital14sable <shital14sable@gmail.com>
2024-03-04 13:47:30 -0800

    Obj O P

--- mlcpn_train.py
+++ mlcpn_train.py
@@ -2,72 +2,80 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 from datetime import date 
-from model import pointnet, generator
+from model import mlcpn, Generator
 from utils import PerLabelMetric, GarbageMan
-from dataset import generate_dataset
+from dataset import DataSet
 import os
 import csv
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
-NUM_POINTS = 2000
-NUM_CLASSES = 25
-TRAINING = True
-LEARN_RATE = 0.0003
-BATCH_SIZE = 32
-NUM_EPOCHS = 25
-username = 'Zachariah'
-database = "AFBMData_NoChairs_Augmented.csv"
-save_path = str('/mnt/c/Users/' + username +'/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AFBMGit/AFBM_TF_DATASET/' + str(date.today()) + '_' + str(BATCH_SIZE) + '_' + str(NUM_POINTS) + '_' + str(NUM_EPOCHS) + '_' + 'Learning Rate_' + str(LEARN_RATE))
-
-
-train_ds, val_ds, label_weights = generate_dataset(filename=database)
-with open("Label_Weights.csv", mode='w') as f:
-    writer = csv.writer(f)
-    for key, value in label_weights.items():
-        writer.writerow([key, value])
-
-pn = pointnet(num_classes=NUM_CLASSES,num_points=NUM_POINTS,train=TRAINING)
-pn.compile(
-    loss=tf.keras.losses.BinaryCrossentropy(),
-    optimizer=tf.keras.optimizers.Adam(learning_rate=LEARN_RATE),
-    metrics=[
+class mlcpn_train:
+    def __init__(self, num_points = 2000, classes=25, training=True, lr = 0.0003, batch_size=32, epochs=25, username='Zachariah', database=  "AFBMData_NoChairs_Augmented.csv", sav_path = 'C:/Data/PointNetWork/Output_files/') -> None:
+        self.num_points_ = num_points
+        self.num_classes_ = classes
+        self.training_ = True
+        self.learn_rate_ = lr
+        self.batch_size_ = batch_size
+        self.num_epochs_ = epochs
+        self.username_ = 'Zachariah'
+        self.database_ =  database
+        self.save_path_ = str(sav_path + str(date.today()) + '_' + str(self.batch_size_) + '_' + str(self.num_points_) + '_' + str(self.num_epochs_) + '_' + 'Learning Rate_' + str(self.learn_rate_))
+        
+    def training(self) -> int:
+        ds1 = DataSet()
+        train_ds, val_ds, label_weights = ds1.generate_dataset(filename=self.database_)
+        with open("Label_Weights.csv", mode='w') as f:
+            writer = csv.writer(f)
+            for key, value in label_weights.items():
+                writer.writerow([key, value])
+        pn = mlcpn(self.num_classes_, self.num_points_, self.training_)
+        pn_model = pn.pointnet()
+        pn_model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=self.learn_rate_),
+        metrics=[
             #PerLabelMetric(num_labels=NUM_CLASSES),
             tf.keras.metrics.BinaryAccuracy(threshold=0.5),
             tf.keras.metrics.Precision(thresholds=[0.5,1]),
             tf.keras.metrics.Recall(thresholds=[0.5,1]),
             ],      
-    run_eagerly=True,
-)
-
-tist = pn.fit(x=train_ds, epochs=NUM_EPOCHS, class_weight=label_weights, validation_data=val_ds, callbacks=[GarbageMan()])
-pn.save(save_path + '_AFBM Model')
-
-## Save history file
-histdf = pd.DataFrame(tist.history[0]).T
-histfile = save_path + '_train_history_per_label_met.csv'
-with open(histfile, mode='w') as f:
-    histdf.to_csv(f)
-
-histdf = pd.DataFrame(tist.history[1:])
-histfile = save_path + '_train_history.csv'
-with open(histfile, mode='w') as f:
-    histdf.to_csv(f)
-
-# Validation / Evaluation per Label
-data = []
-pn.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=LEARN_RATE),
-    metrics=[
-        PerLabelMetric(num_labels=NUM_CLASSES),
-        ],
         run_eagerly=True,
-    )
-data=pn.evaluate(x=val_ds)
-metrics = data[1]
-metrics = pd.DataFrame(metrics).T
-print(metrics)
-histfile = save_path + '_label_validation_allmets_2.csv'
+        )
 
-with open(histfile, mode='w') as f:
-    metrics.to_csv(f)
+        pn_model.fit(x=train_ds, epochs=self.num_epochs_, class_weight=label_weights, validation_data=val_ds, callbacks=[GarbageMan()])
+        pn_model.save(self.save_path_ + '_AFBM Model')
+
+        ## Save history file
+        histdf = pd.DataFrame(pn_model.history[0]).T
+        histfile = self.save_path_ + '_train_history_per_label_met.csv'
+        with open(histfile, mode='w') as f:
+            histdf.to_csv(f)
+
+        histdf = pd.DataFrame(pn_model.history[1:])
+        histfile = self.save_path_ + '_train_history.csv'
+        with open(histfile, mode='w') as f:
+            histdf.to_csv(f)
+
+        # Validation / Evaluation per Label
+        data = []
+        pn_model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate= self.learn_rate_),
+            metrics=[
+                PerLabelMetric(num_labels=self.num_classes_),
+                ],
+                run_eagerly=True,
+            )
+        data=pn_model.evaluate(x=val_ds)
+        metrics = data[1]
+        metrics = pd.DataFrame(metrics).T
+        print(metrics)
+        histfile = self.save_path_ + '_label_validation_allmets_2.csv'
+
+        with open(histfile, mode='w') as f:
+            metrics.to_csv(f)
+
+
+if __name__ == '__main__':
+    model_train = mlcpn_train(2000, 25, True, 0.0003, 32, 25, 'Zachariah', database=  "/mnt/c/Data/PointNetWork/AFBMData_NoChairs_Augmented.csv", sav_path='/mnt/c/Data/PointNetWork/Output_files/')
+    model_train.training()
+    
--- model.py
+++ model.py
@@ -5,20 +5,109 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import layers
 
-def conv_bn(x, filters, stride=1, train=True):
-    x = layers.Conv1D(filters, strides=stride, kernel_size=1, padding="valid")(x)
-    x = layers.BatchNormalization(momentum=0.0, trainable=train)(x)
-    return layers.Activation('LeakyReLU')(x)
+class mlcpn:
+    def __init__(self, classes, num_points, training= True) -> None:
+        self.num_classes = classes
+        self.num_points = num_points
+        self.training = training
 
-def dconv_bn(x, filters, stride=1, train=True):
-    x = layers.Conv1DTranspose(filters, kernel_size=1, strides=stride, padding="valid")(x)
-    x = layers.BatchNormalization(momentum=0.0, trainable=train)(x)
-    return layers.Activation('LeakyReLU')(x)
+    def conv_bn(self, x, filters, stride=1, train=True):
+        x = layers.Conv1D(filters, strides=stride, kernel_size=1, padding="valid")(x)
+        x = layers.BatchNormalization(momentum=0.0, trainable=train)(x)
+        return layers.Activation('LeakyReLU')(x)
 
-def dense_bn(x, filters, train=True):
-    x = layers.Dense(filters)(x)
-    x = layers.BatchNormalization(momentum=0.0, trainable=train)(x)
-    return layers.Activation('LeakyReLU')(x)
+    def dense_bn(self, x, filters, train=True):
+        x = layers.Dense(filters)(x)
+        x = layers.BatchNormalization(momentum=0.0, trainable=train)(x)
+        return layers.Activation('LeakyReLU')(x)
+    
+    def orthogonal_regularizer_from_config(self, config):
+        return OrthogonalRegularizer(**config)
+
+    def tnet(self, inputs, num_features, train=True):
+        bias = keras.initializers.Constant(np.eye(num_features).flatten())
+        reg = OrthogonalRegularizer(num_features)
+        x = self.conv_bn(inputs, 64, train=train)
+        x = self.conv_bn(x, 128, train=train)
+        x = self.conv_bn(x, 1024, train=train)
+        x = layers.GlobalMaxPooling1D()(x)
+        x = self.dense_bn(x, 512, train=train)
+        x = self.dense_bn(x, 256, train=train)
+        x = layers.Dense(
+            num_features * num_features,
+            kernel_initializer="zeros",
+            bias_initializer=bias,
+            activity_regularizer=reg)(x)
+        feat_T = layers.Reshape((num_features, num_features))(x)
+        return layers.Dot(axes=(2, 1))([inputs, feat_T])
+
+    def pointnet(self):
+        """
+        Returns Keras MLC-PointNet Implementation
+        Args:
+            training (boolean): Training or not
+            num_points (scalar): Number of points in the point cloud
+        """    
+        inputs = keras.Input(shape=(self.num_points, 3))
+        x = self.tnet(inputs, 3, train=self.training)
+        x = self.conv_bn(x, 64, train=self.training)
+        x = self.conv_bn(x, 64, train=self.training)
+        x = self.tnet(x, 64, train=self.training)
+        x = self.conv_bn(x, 64, train=self.training)
+        x = self.conv_bn(x, 128, train=self.training)
+        x = self.conv_bn(x, 1024, train=self.training)
+        x = layers.GlobalMaxPooling1D()(x)
+        x = self.dense_bn(x, 512, train=self.training)
+        x = self.dense_bn(x, 256, train=self.training)
+        outputs = layers.Dense(self.num_classes, activation="sigmoid")(x)
+        model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
+        return model
+
+class Generator:
+    def __init__(self, num_points, num_classes, train) -> None:
+        self.num_classes = num_classes
+        self.num_points = num_points
+        self.training = train
+
+        
+    def conv_bn(self, x, filters, stride=1, train=True):
+        x = layers.Conv1D(filters, strides=stride, kernel_size=1, padding="valid")(x)
+        x = layers.BatchNormalization(momentum=0.0, trainable=train)(x)
+        return layers.Activation('LeakyReLU')(x)
+
+    def dconv_bn(self, x, filters, stride=1, train=True):
+        x = layers.Conv1DTranspose(filters, kernel_size=1, strides=stride, padding="valid")(x)
+        x = layers.BatchNormalization(momentum=0.0, trainable=train)(x)
+        return layers.Activation('LeakyReLU')(x)
+
+    def dense_bn(self, x, filters, train=True):
+        x = layers.Dense(filters)(x)
+        x = layers.BatchNormalization(momentum=0.0, trainable=train)(x)
+        return layers.Activation('LeakyReLU')(x)
+
+    def generator(self):
+        """
+        Returns Keras C-GAN
+        Args:
+            training (boolean): Training or not
+            num_points (integer): Number of points in the point cloud
+            num_classes (integer): Number of classes         
+        """    
+        input = keras.Input(shape=(self.num_classes,1))
+        x = self.conv_bn(input, 1028, train=self.training)
+        x = self.conv_bn(x, 512, train=self.training)
+        x = self.conv_bn(x, 256, train=self.training)
+        x = self.conv_bn(x, 128, train=self.training)
+        x = self.conv_bn(x, 1, stride=self.num_classes, train=self.training)
+        x = self.dconv_bn(x, 1, stride= self.num_points, train=self.training)
+        x = self.dconv_bn(x,  self.num_points/8, train=self.training)
+        x = self.dconv_bn(x,  self.num_points/4, train=self.training)
+        x = self.dconv_bn(x,  self.num_points/2, train=self.training)
+        x = self.dconv_bn(x,  self.num_points, train=self.training)
+        #x = layers.Reshape((5000,3))(x)
+        x = layers.Dense(3, activation="sigmoid")(x)
+        model = keras.Model(inputs=input, outputs=x, name="c_gan")
+        return model
 
 class OrthogonalRegularizer(keras.regularizers.Regularizer):
     def __init__(self, num_features=None, l2reg=0.001, **kwargs):
@@ -40,69 +129,3 @@ class OrthogonalRegularizer(keras.regularizers.Regularizer):
     def from_config(cls, config):
         return cls(num_features=config.pop('num_features', None), **config)
     
-def orthogonal_regularizer_from_config(config):
-    return OrthogonalRegularizer(**config)
-
-def tnet(inputs, num_features, train=True):
-    bias = keras.initializers.Constant(np.eye(num_features).flatten())
-    reg = OrthogonalRegularizer(num_features)
-    x = conv_bn(inputs, 64, train=train)
-    x = conv_bn(x, 128, train=train)
-    x = conv_bn(x, 1024, train=train)
-    x = layers.GlobalMaxPooling1D()(x)
-    x = dense_bn(x, 512, train=train)
-    x = dense_bn(x, 256, train=train)
-    x = layers.Dense(
-        num_features * num_features,
-        kernel_initializer="zeros",
-        bias_initializer=bias,
-        activity_regularizer=reg,
-    )(x)
-    feat_T = layers.Reshape((num_features, num_features))(x)
-    return layers.Dot(axes=(2, 1))([inputs, feat_T])
-
-def pointnet(num_points, num_classes, train=True):
-    """
-    Returns Keras MLC-PointNet Implementation
-    Args:
-        training (boolean): Training or not
-        num_points (scalar): Number of points in the point cloud
-    """    
-    inputs = keras.Input(shape=(num_points, 3))
-    x = tnet(inputs, 3, train=train)
-    x = conv_bn(x, 64, train=train)
-    x = conv_bn(x, 64, train=train)
-    x = tnet(x, 64, train=train)
-    x = conv_bn(x, 64, train=train)
-    x = conv_bn(x, 128, train=train)
-    x = conv_bn(x, 1024, train=train)
-    x = layers.GlobalMaxPooling1D()(x)
-    x = dense_bn(x, 512, train=train)
-    x = dense_bn(x, 256, train=train)
-    outputs = layers.Dense(num_classes, activation="sigmoid")(x)
-    model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
-    return model
-
-def generator(num_points, num_classes, train=True):
-    """
-    Returns Keras C-GAN
-    Args:
-        training (boolean): Training or not
-        num_points (integer): Number of points in the point cloud
-        num_classes (integer): Number of classes         
-    """    
-    input = keras.Input(shape=(num_classes,1))
-    x = conv_bn(input, 1028, train=train)
-    x = conv_bn(x, 512, train=train)
-    x = conv_bn(x, 256, train=train)
-    x = conv_bn(x, 128, train=train)
-    x = conv_bn(x, 1, stride=num_classes, train=train)
-    x = dconv_bn(x, 1, stride=num_points, train=train)
-    x = dconv_bn(x, num_points/8, train=train)
-    x = dconv_bn(x, num_points/4, train=train)
-    x = dconv_bn(x, num_points/2, train=train)
-    x = dconv_bn(x, num_points, train=train)
-    #x = layers.Reshape((5000,3))(x)
-    x = layers.Dense(3, activation="sigmoid")(x)
-    model = keras.Model(inputs=input, outputs=x, name="c_gan")
-    return model
