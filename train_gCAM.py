d418f4e4bec9f6d5ced3bc6fe6ab170fefa0b967
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-03-06 15:35:54 -0800

    Fixed stuff

--- train_gCAM.py
+++ train_gCAM.py
@@ -15,9 +15,9 @@ EPS = 1e-7
 NUM_POINTS = 5000
 NUM_CLASSES = 25
 TRAINING = True
-LEARN_RATE = 0.25
+LEARN_RATE = 0.00025
 BATCH_SIZE = 16
-NUM_EPOCHS = 18
+NUM_EPOCHS = 25
 username = 'Zachariah'
 database = "AFBMData_NoChairs_Augmented.csv"
 save_path = str('/mnt/c/Users/' + username +'/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AFBMGit/AFBM_TF_DATASET/MLCPN_Validation' + str(date.today()) + '_' + str(BATCH_SIZE) + '_' + str(NUM_POINTS) + '_' + str(NUM_EPOCHS) + '_' + 'Learning Rate_' + str(LEARN_RATE) + '_' + 'Epsilon: ' + str(EPS))
@@ -27,7 +27,7 @@ pn_model = pointnet(num_points=NUM_POINTS, num_classes=NUM_CLASSES, train=True)
 #print(pn_model.get_weights()[0])
 #print(pn_model.get_weights()[1])
 EStop = EarlyStopping(monitor='val_loss',patience=3, mode='min')
-patience = 0
+patience = 5
 echeck = 0
 ediff = 0.001
 cur_loss = 0.0
