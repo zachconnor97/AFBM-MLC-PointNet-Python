e5a53410a935b16ede7b7cf2b9ecb961b6f53745
Gabriel Connor <83265255+BlendedPotato@users.noreply.github.com>
2024-03-07 09:53:01 -0800

    testing

--- TensorTesting.py
+++ TensorTesting.py
@@ -1,15 +1,9 @@
 import tensorflow as tf
 import numpy as np
 #tf.config.run_functions_eagerly(False)
-tensor_array = tf.TensorArray(dtype=tf.Tensor, size=0,dynamic_size=True)
-stringy = ['string1', 'string2','string3','string4','string5']
-t = tf.constant(stringy,dtype=tf.string)
-tensor_array = tensor_array.write(0,t)
-print(tensor_array)
-print(t)
-print(type(t))
-#t = tf.strings.to_number(t)
-#ts = t.numpy()
 
-test = tf.strings.as_string([3,2])
-print(test.numpy())
+x = tf.constant([5, 4, 6])
+y = tf.constant([5, 2, 5])
+tab = tf.math.greater(x, y)
+
+print(tab)
