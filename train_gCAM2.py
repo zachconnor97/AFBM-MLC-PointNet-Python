054d5e7b2c4081606171ab7327ea63ed52d48fba
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-03-18 15:01:44 -0700

    Updated

--- train_gCAM2.py
+++ train_gCAM2.py
@@ -187,7 +187,9 @@ def gradcam_heatcloud(cloud, model, lcln, label_idx=None):
         if label_idx is None:
             label_idx = tf.argmax(preds[0])
         label_channel = preds[:, label_idx]
+    #print(label_idx)
     grads = tape.gradient(label_channel, lclo)
+    #print(grads)
     #pooled_grads = tf.reduce_mean(grads, axis=0) # Dimensionality of this var is causing issue in line 190...
     pooled_grads = tf.reduce_mean(grads, axis=(0,1)) 
     #print(pooled_grads[..., tf.newaxis])
@@ -214,6 +216,7 @@ def save_and_display_gradcam(point_cloud, heatcloud):
     #rg[:,0] = np.subtract(rg[:,0], (1 + np.log(heatcloud)))
     #rg[:,1] = np.subtract(rg[:,1], (1 + np.log(heatcloud)))
     #b = np.ones((len(heatcloud),1))
+
     g = 1 + np.log(heatcloud)
     g = np.reshape(g, (len(heatcloud),1))
     rgb = np.hstack((v,g,v))
@@ -227,10 +230,13 @@ def save_and_display_gradcam(point_cloud, heatcloud):
 
 # Test GradCAM stuff
 pn_model.load_weights('MLCPNBestWeights.h5')
+#pn_model.summary()
 #testcloud = o3d.io.read_point_cloud('C:/Users/gabri/OneDrive - Oregon State University/AllClouds10k/AllClouds10k/lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply') # use open3d to import point cloud from file
 #testcloud = o3d.io.read_point_cloud('C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/bottle_2876657_2618100a5821a4d847df6165146d5bbd1_10000_2pc.ply') # use open3d to import point cloud from file
 #testcloud = o3d.io.read_point_cloud('/mnt/c/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply') # use open3d to import point cloud from file
-pc_path = 'C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply'
+pc_path = 'C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/sofa_couch_lounge_4256520_3e3ad2629c9ab938c2eaaa1f79e71ec1_10000_2pc.ply'
+#bottle_2876657_2618100a5821a4d847df6165146d5bbd1_10000_2pc.ply'
+#lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply'
 pc = o3d.io.read_point_cloud(pc_path)
 pc = pc.uniform_down_sample(every_k_points=2)
 pc= pc.points
@@ -238,10 +244,11 @@ pc = np.asarray([pc])[0]
 testcloud = np.reshape(pc, (1,5000,3))
 testcloud = tf.constant(testcloud, dtype='float64')
 pn_model.layers[-1].activation = None
-lln = 'activation_14' 
-labels = pn_model.predict(testcloud.numpy())
+lln = 'activation_14' #'conv1d_10'
+labels = pn_model.predict(testcloud)
 print("Predicted Labels: ", labels)
-heatcloud = gradcam_heatcloud(testcloud, pn_model, lln)
-save_and_display_gradcam(pc, heatcloud)
+heatcloud = gradcam_heatcloud(testcloud, pn_model, lln, label_idx=24)
+print(heatcloud)
+#save_and_display_gradcam(pc, heatcloud)
 
 
