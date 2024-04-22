61a917506e8588f8eca6e27a4bd686a2768dc2ae
zachconnor97 <62565704+zachconnor97@users.noreply.github.com>
2024-04-21 18:39:39 -0700

    Cleaned up the code

--- gCAM_Display.py
+++ gCAM_Display.py
@@ -12,6 +12,8 @@ from tensorflow import keras
 from IPython.display import Image, display
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import colorsys
+import matplotlib
 from model import pointnet, OrthogonalRegularizer, orthogonal_regularizer_from_config
 from utils import PerLabelMetric, GarbageMan
 from dataset_example import generate_dataset
@@ -41,7 +43,6 @@ label_names = [
 ]
 
 g_optimizer = tf.keras.optimizers.Adam(learning_rate=LEARN_RATE)
-
 
 def gradcam_heatcloud(cloud, model, lcln, label_idx=None):
     """
@@ -74,66 +75,18 @@ def gradcam_heatcloud(cloud, model, lcln, label_idx=None):
 def save_and_display_gradcam(point_cloud, heatcloud, result_path, fileid, i=None, label_names=None):
 
     pc = point_cloud
-    v = np.zeros((len(heatcloud),1))
-    #rg[:,0] = np.subtract(rg[:,0], (1 + np.log(heatcloud)))
-    #rg[:,1] = np.subtract(rg[:,1], (1 + np.log(heatcloud)))
-    #b = np.ones((len(heatcloud),1))
-
-    #g = 1 + np.log(heatcloud)
-    h = heatcloud
+    o = np.ones((len(heatcloud),1))
+    h = (2/3) * (1 - heatcloud)
     h = np.reshape(h, (len(heatcloud),1))
-    ratio = 2 * h
-    blue = np.maximum(v, (1-ratio))
-    red = np.maximum(v, (ratio-1))
-    green = 1.0 - blue - red
-    rgb = np.hstack((red, green, blue))
+    hsv = np.hstack((h,o,o))
+    rgb = mpl.colors.hsv_to_rgb(hsv)
     # Convert back to open3d pc
     cloud = o3d.geometry.PointCloud()
     cloud.points = o3d.utility.Vector3dVector(pc)
     cloud.colors = o3d.utility.Vector3dVector(rgb)
     o3d.visualization.draw_geometries([cloud])
-    """
-    try:
-        o3d.io.write_point_cloud(result_path + fileid + "Point_Cloud_Intensity" + label_names[i] + ".ply", cloud)
-    except:
-        print("cloud not written")
 
-    
- 
-    vis = o3d.visualization.Visualizer()
-    vis.create_window()
-    vis.add_geometry(cloud)
-    
-    vis.get_render_option().point_size = 4
-    vis.get_render_option().background_color = np.asarray([1, 1, 1])  
-    
-    ctr = vis.get_view_control()
-    ctr.rotate(180.0, 180.0)  
-    
-
-    vis.update_geometry(cloud)
-    vis.poll_events()
-    vis.update_renderer()
-    
-    vis.capture_screen_image(result_path + fileid + "Point_Cloud_Intensity" + label_names[i] + ".png")
-    
-    vis.destroy_window()
-    """
-# Test GradCAM stuff
-
-
-
-#pn_model.summary()
-#testcloud = o3d.io.read_point_cloud('C:/Users/gabri/OneDrive - Oregon State University/AllClouds10k/AllClouds10k/lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply') # use open3d to import point cloud from file
-#testcloud = o3d.io.read_point_cloud('C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/bottle_2876657_2618100a5821a4d847df6165146d5bbd1_10000_2pc.ply') # use open3d to import point cloud from file
-#testcloud = o3d.io.read_point_cloud('/mnt/c/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply') # use open3d to import point cloud from file
-#pc_path = 'C:/Users/gabri/OneDrive - Oregon State University/AllClouds10k/AllClouds10k/vessel_watercraft_4530566_6c9020061d71b190a4755e7555b1e1a43_10000_2pc.ply'
-pc_path = 'C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/bottle_2876657_2618100a5821a4d847df6165146d5bbd1_10000_2pc.ply' #sofa_couch_lounge_4256520_3e3ad2629c9ab938c2eaaa1f79e71ec1_10000_2pc.ply'
-#mug_3797390_3a7439cfaa9af51faf1af397e14a566d3_10000_2pc.ply' 
-#lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply' #'sofa_couch_lounge_4256520_3e3ad2629c9ab938c2eaaa1f79e71ec1_10000_2pc.ply'
-#pc_path = "C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AFBMGit/AFBM_TF_DATASET/gcam_results/slicing study/green_lamp_3636649_199273d17414e77ca553fc23769e60511_10000_2pcPoint_Cloud_IntensityConvertEEtoLE.ply"
-#bottle_2876657_2618100a5821a4d847df6165146d5bbd1_10000_2pc.ply'
-#lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply'
+pc_path = 'C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/lamp_3636649_ca968e46ba74732551970742dd5663211_10000_2pc.ply' 
 pc = o3d.io.read_point_cloud(pc_path)
 #pc = pc.uniform_down_sample(every_k_points=2)
 pc = pc.points
@@ -145,11 +98,6 @@ testcloud = tf.constant(testcloud, dtype='float64')
 pn_model = pointnet(num_points=NUM_POINTS, num_classes=NUM_CLASSES, train=True)
 pn_model.load_weights('MLCPNBestWeights.h5') #'MLCPN_Validation_New2024-04-01_16_5000_30_Learning Rate_0.00025_Epsilon_1e-07pn_weights_25.h5') #
 pn_model.compile(run_eagerly=True)
-"""
-database = "AFBMData_NoChairs_Augmented.csv"
-train_ds, val_ds, label_weights, val_paths = generate_dataset(filename=database)
-
-"""
 lln = 'conv1d_10' #'dense_7' #'dot_1'
 y_pred = pn_model.predict(testcloud)
 label_names = np.array(label_names)
@@ -159,47 +107,14 @@ output = label_names
 print(f"Prdicted Labels: \n")
 for i in range(0, len(output)):
     output[i] = output[i] + ": " + str(round(y_pred1[i], 5))
-    if y_pred1[i] >= 0.2:
+    if y_pred1[i] >= 0.5:
         print(f"Label {i}: {output[i]}")
 #label_dict = pd.concat(pd.DataFrame(label_names),pd.DataFrame(y_pred))
 pn_model.layers[-1].activation = None
 
-"""
-example_clouds = val_ds.take(BATCH_SIZE)
-example_clouds = example_clouds.batch(BATCH_SIZE)
-example_paths = val_paths.take(BATCH_SIZE)
-points, y_true = list(example_clouds)[0]
-y_pred = pn_model.predict(example_clouds, batch_size=BATCH_SIZE)
-paths = list(example_paths)
-lln = 'conv1d_10' #'dot_1' #'activation_14' #
-pn_model.layers[-1].activation = None
-result_path = "C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AFBMGit/AFBM_TF_DATASET/gcam_results/"
-
-for j in range(len(y_pred)):
-    print(f"Getting GradCAM for Point Cloud {j}")
-    #print(f"Y Predicted: {type(y_pred[i])}")
-    #print(f"Y Predicted Size: {np.shape(y_pred[i])}")
-    yp = y_pred[j]
-    yt = y_true[j]
-    p = points[j]
-    fileid = np.array2string(paths[j].numpy())
-    fileid = fileid.split(".")[0]
-    for character in "['":
-        fileid = fileid.replace(character, '')
-    fileid = fileid[1:]
-    pred_label_index = np.where(yp >= 0.5)[0]
-    
-    for i in pred_label_index:
-        heatcloud = gradcam_heatcloud(p, pn_model, lln, label_idx=i)
-        #print(heatcloud)
-        save_and_display_gradcam(p, heatcloud, result_path, fileid, i, label_names)
-        
-
-
-"""
 # Get idx from predicted labels with prediction > 0.5 
 y_pred = np.array(y_pred)
-pred_label_index = np.where(y_pred >= 0.2)[1]
+pred_label_index = np.where(y_pred >= 0.5)[1]
 #print(label_index)
 
 for i in pred_label_index:
--- gCAM_Display2.py
+++ gCAM_Display2.py
@@ -12,7 +12,6 @@ from tensorflow import keras
 from IPython.display import Image, display
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-
 from model import pointnet, OrthogonalRegularizer, orthogonal_regularizer_from_config
 from utils import PerLabelMetric, GarbageMan
 from dataset_example import generate_dataset
@@ -42,7 +41,6 @@ label_names = [
 ]
 
 g_optimizer = tf.keras.optimizers.Adam(learning_rate=LEARN_RATE)
-
 
 def gradcam_heatcloud(cloud, model, lcln, label_idx=None):
     """
@@ -75,102 +73,37 @@ def gradcam_heatcloud(cloud, model, lcln, label_idx=None):
 def save_and_display_gradcam(point_cloud, heatcloud, result_path, fileid, i=None, label_names=None):
 
     pc = point_cloud
-    #print(f"Point Cloud Length {len(pc)}")
-    #rg[:,0] = np.subtract(rg[:,0], (1 + np.log(heatcloud)))
-    #rg[:,1] = np.subtract(rg[:,1], (1 + np.log(heatcloud)))
-    #b = np.ones((len(heatcloud),1))
-    #print(i)
-    #repeat_factor = int(len(pc) / len(heatcloud)) + 1
-    #print(repeat_factor)
-    #heatcloud = np.repeat(heatcloud, repeat_factor)
-    #pc_length = len(pc)
-    #heatcloud = heatcloud[0:pc_length]
-    #print(f"Repeated Heatcloud Length: {len(heatcloud)}")
-    #g = 1 + np.log(heatcloud)
-    g = heatcloud
-    g = np.reshape(g, (len(heatcloud),1))
-    v = np.zeros((len(heatcloud),1))
-    rgb = np.hstack((v,g,v))
+    o = np.ones((len(heatcloud),1))
+    h = (2/3) * (1 - heatcloud)
+    h = np.reshape(h, (len(heatcloud),1))
+    hsv = np.hstack((h,o,o))
+    rgb = mpl.colors.hsv_to_rgb(hsv)
     # Convert back to open3d pc
     cloud = o3d.geometry.PointCloud()
     cloud.points = o3d.utility.Vector3dVector(pc)
     cloud.colors = o3d.utility.Vector3dVector(rgb)
     #o3d.visualization.draw_geometries([cloud])
-
-    
     try:
         o3d.io.write_point_cloud(result_path + fileid + "Point_Cloud_Intensity" + label_names[i] + ".ply", cloud)
     except:
         print("cloud not written")
-
-    
-    
     vis = o3d.visualization.Visualizer()
     vis.create_window()
     vis.add_geometry(cloud)
-    
     vis.get_render_option().point_size = 4
     vis.get_render_option().background_color = np.asarray([1, 1, 1])  
-    
     ctr = vis.get_view_control()
     ctr.rotate(180.0, 180.0)  
-    
-
     vis.update_geometry(cloud)
     vis.poll_events()
     vis.update_renderer()
-    
     vis.capture_screen_image(result_path + fileid + "Point_Cloud_Intensity" + label_names[i] + ".png")
-    
     vis.destroy_window()
     
-# Test GradCAM stuff
-
-"""
-
-#pn_model.summary()
-#testcloud = o3d.io.read_point_cloud('C:/Users/gabri/OneDrive - Oregon State University/AllClouds10k/AllClouds10k/lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply') # use open3d to import point cloud from file
-#testcloud = o3d.io.read_point_cloud('C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/bottle_2876657_2618100a5821a4d847df6165146d5bbd1_10000_2pc.ply') # use open3d to import point cloud from file
-#testcloud = o3d.io.read_point_cloud('/mnt/c/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply') # use open3d to import point cloud from file
-#pc_path = 'C:/Users/gabri/OneDrive - Oregon State University/AllClouds10k/AllClouds10k/vessel_watercraft_4530566_6c9020061d71b190a4755e7555b1e1a43_10000_2pc.ply'
-#pc_path = 'C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply' #'sofa_couch_lounge_4256520_3e3ad2629c9ab938c2eaaa1f79e71ec1_10000_2pc.ply'
-#pc_path = "C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AFBMGit/AFBM_TF_DATASET/gcam_results/slicing study/green_lamp_3636649_199273d17414e77ca553fc23769e60511_10000_2pcPoint_Cloud_IntensityConvertEEtoLE.ply"
-#pc_path = 'C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/guitar_3467517_b6f78c76441e30fbb60d315a520dce5d3_10000_2pc.ply' #bottle_2876657_2618100a5821a4d847df6165146d5bbd1_10000_2pc.ply'
-pc_path = "C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AllClouds10k/AllClouds10k/bottle_2876657_523cddb320608c09a37f3fc1915517003_10000_2pc.ply" #jar_3593526_9e8595c3bbb0a5d0ba5342d638d0c2671_10000_2pc.ply" #mug_3797390_3a7439cfaa9af51faf1af397e14a566d3_10000_2pc.ply"
-#lamp_3636649_be13324c84d2a9d72b151d8b52c53b901_10000_2pc.ply'
-pc = o3d.io.read_point_cloud(pc_path)
-#pc = pc.uniform_down_sample(every_k_points=2)
-pc = pc.points
-pc = np.asarray([pc])[0]
-NUM_POINTS = len(pc)
-testcloud = np.reshape(pc, (1,NUM_POINTS,3))
-testcloud = tf.constant(testcloud, dtype='float64')
-
-pn_model = pointnet(num_points=NUM_POINTS, num_classes=NUM_CLASSES, train=True)
-pn_model.load_weights('MLCPN_Validation_New2024-04-01_16_5000_30_Learning Rate_0.00025_Epsilon_1e-07pn_weights_5.h5') #'MLCPNBestWeights.h5') #
-pn_model.compile(run_eagerly=True)
-"""
 database = "AFBMData_NoChairs_Augmented.csv"
 train_ds, val_ds, label_weights, val_paths = generate_dataset(filename=database)
-
-"""
-lln = 'conv1d_10' #'dense_8' #'dot_1'
-y_pred = pn_model.predict(testcloud)
-label_names = np.array(label_names)
-y_pred1 = y_pred.tolist()[0]
-label_names = label_names.tolist()
-output = label_names
-#print(f"Prdicted Labels: \n")
-for i in range(0, len(output)):
-    output[i] = output[i] + ": " + str(round(y_pred1[i], 5))
-    #if y_pred1[i] >= 0.2:
-        #print(f"Label {i}: {output[i]}")
-#label_dict = pd.concat(pd.DataFrame(label_names),pd.DataFrame(y_pred))
-#pn_model.layers[-1].activation = None
-
-"""
 pn_model = pointnet(num_points=NUM_POINTS, num_classes=NUM_CLASSES, train=False)
-pn_model.load_weights('MLCPN_Validation_New2024-04-01_16_5000_30_Learning Rate_0.00025_Epsilon_1e-07pn_weights_25.h5') #'MLCPNBestWeights.h5') #
+pn_model.load_weights('MLCPNBestWeights.h5') #'MLCPN_Validation_New2024-04-01_16_5000_30_Learning Rate_0.00025_Epsilon_1e-07pn_weights_25.h5') #
 pn_model.compile(run_eagerly=True)
 example_clouds = val_ds.take(BATCH_SIZE)
 example_clouds = example_clouds.batch(BATCH_SIZE)
@@ -181,11 +114,8 @@ paths = list(example_paths)
 lln = 'conv1d_10' #'dot_1' #'activation_14' #
 pn_model.layers[-1].activation = None
 result_path = "C:/Users/Zachariah/OneDrive - Oregon State University/Research/AFBM/AFBM Code/AFBMGit/AFBM_TF_DATASET/gcam_results/"
-
 for j in range(len(y_pred)):
     print(f"Getting GradCAM for Point Cloud {j}")
-    #print(f"Y Predicted: {type(y_pred[i])}")
-    #print(f"Y Predicted Size: {np.shape(y_pred[i])}")
     yp = y_pred[j]
     yt = y_true[j]
     p = points[j]
@@ -200,17 +130,3 @@ for j in range(len(y_pred)):
         heatcloud = gradcam_heatcloud(p, pn_model, lln, label_idx=i)
         #print(heatcloud)
         save_and_display_gradcam(p, heatcloud, result_path, fileid, i, label_names)
-        
-
-
-"""
-# Get idx from predicted labels with prediction > 0.5 
-y_pred = np.array(y_pred)
-pred_label_index = np.where(y_pred >= 0.2)[1]
-#print(pred_label_index)
-
-for i in pred_label_index:
-    heatcloud = gradcam_heatcloud(testcloud, pn_model, lln, label_idx=i)
-    save_and_display_gradcam(pc, heatcloud, result_path=None, fileid='3797390_3a7439cfaa9af51faf1af397e14a566d3', i=i, label_names=label_names)
-
-"""
