e76fe44df4b54c79635dc669338da329fca830f1
Gabriel Connor <83265255+BlendedPotato@users.noreply.github.com>
2024-05-07 11:33:22 -0700

    Delete TensorTesting.py

--- TensorTesting.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import numpy as np
-#tf.config.run_functions_eagerly(False)
-
-x = tf.constant([5, 4, 6])
-y = tf.constant([5, 2, 5])
-tab = tf.math.greater(x, y)
-
-print(tab)
