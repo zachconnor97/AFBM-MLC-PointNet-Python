bdc90adb3b3153bde1cc73e2dd7e19e2648e4435
BlendedPotato <gabrielconnor01@gmail.com>
2024-05-16 09:18:02 -0700

    Now reads a csv and gets the name while copying the contents to new location

--- copyfiles.py
+++ copyfiles.py
@@ -1,12 +1,30 @@
 import shutil
 import pandas as pd
+import os
+import csv
 
-filename = "C:/Users/gabri/Downloads/output-obj_cloud_out10k1_test.csv"
+filename = "C:/Users/gabri/OneDrive - Oregon State University/Z Research/test.csv"
 
 df = pd.read_csv(filename)
+print(df)
 
-filetocopy = "D:/ZachResearch\MeshlabPython and Files/Objs/ashcan_trash can_garbage can_wastebin_ash bin_ash-bin_ashbin_dustbin_trash barrel_trash bin_2747177_50978d3850fcf4e371a126804ae24042_.obj"
+# Destination directory
+destination_dir = 'C:/Users/gabri/OneDrive - Oregon State University/Z Research/CopyTests'
 
-desination = "D:/ZachResearch/MeshlabPython and Files/Copies/ashcan_trash can_garbage can_wastebin_ash bin_ash-bin_ashbin_dustbin_trash barrel_trash bin_copy_2747177_50978d3850fcf4e371a126804ae24042_.obj"
 
-shutil.copy(filetocopy, desination)
+os.makedirs(destination_dir, exist_ok=True)
+
+
+with open(filename, 'r') as file:
+    reader = csv.reader(file)
+    for path in reader:
+        path = path[0]  
+        if os.path.isfile(path):
+            file_name = os.path.basename(path)  
+            destination_file = os.path.join(destination_dir, file_name)
+            
+            # Copy file from source to destination
+            shutil.copy(path, destination_file)
+            print(f"File {file_name} copied successfully.")
+        else:
+            print(f"File {path} does not exist.")
